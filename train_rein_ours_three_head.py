08fada809d3ea80cfe415b9c0d9418706a84da43
birdomi <ryk012@naver.com>
2024-05-04 21:44:49 +0900

    add cifar for our code

--- train_rein_ours_three_head.py
+++ train_rein_ours_three_head.py
@@ -47,7 +47,8 @@ def train():
         train_loader, valid_loader = utils.get_mnist_noise_dataset(args.data, noise_rate=noise_rate, batch_size = batch_size)
     elif args.data == 'dr':
         train_loader, valid_loader = utils.get_dr(data_path, batch_size = batch_size)
-    
+    elif 'cifar' in args.data:
+        train_loader, valid_loader = utils.get_cifar_noise_dataset(data_path, batch_size = batch_size,  noise_rate=noise_rate)
     
     num_samples = {}
     for i in range(config['num_classes']):
